14fe0461bd0ecf703efa8735bf05578534cc90be
yuedong56 <865812052@126.com>
2014-12-11 11:51:57 +0800

    feedback

--- LaoLuoYuLu/LaoLuoYuLu/Classes/UIs/Setting/SuggestViewController.m
+++ LaoLuoYuLu/LaoLuoYuLu/Classes/UIs/Setting/SuggestViewController.m
@@ -5,11 +5,12 @@
 //  Created by 老岳 on 14-12-6.
 //  Copyright (c) 2014年 LYue. All rights reserved.
 //
+//  http://m.weibo.cn/1905373741/BA3Ag5WPO
 
 #import "SuggestViewController.h"
 
-@interface SuggestViewController ()
-
+@interface SuggestViewController ()<UIWebViewDelegate>
+@property (nonatomic, strong) UIWebView *weiboWebView;
 @end
 
 @implementation SuggestViewController
@@ -21,6 +22,7 @@
     self.view.backgroundColor = WhiteColor;
     
     [self setNavStyle];
+    [self loadingLaoLuoWeiBoWebView];
 }
 
 /** 设置导航栏样式 */
@@ -39,6 +41,27 @@
     
     self.automaticallyAdjustsScrollViewInsets = NO;
 }
+
+/** 加载老罗的微博页面 */
+- (void)loadingLaoLuoWeiBoWebView
+{
+    if (!self.weiboWebView) {
+        [self initWebView];
+    }
+    NSURL *url = [NSURL URLWithString:@"http://m.weibo.cn/1905373741/BA3Ag5WPO"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.weiboWebView loadRequest:request];
+}
+
+/** 初始化webview */
+- (void)initWebView
+{
+    self.weiboWebView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 64, ScreenWidth, ScreenHeight-64)];
+    self.weiboWebView.delegate = self;
+    self.weiboWebView.backgroundColor = WhiteColor;
+    [self.view addSubview:self.weiboWebView];
+}
+
 
 @end
 
